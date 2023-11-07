14694a22ecf07080c34e88d834abe937d18e5d2e
Rayane <rayane.belbachir@student.vinci.be>
2023-11-07 12:34:50 +0100

    modif usernam ligne 8

--- api/routes/auths.js
+++ api/routes/auths.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* Register a user */
 router.post('/register', async (req, res) => {
-  const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
+  const usernam = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
   if (!username || !password) return res.sendStatus(400); // 400 Bad Request
