736f5981fe5f52f3a20cbc2a21c1eec57fed531e
Rayane <rayane.belbachir@student.vinci.be>
2023-11-21 00:08:34 +0100

    add : uri LoginPage and RegisterPage

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,13 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
-import NewPage from '../Pages/NewPage';
+import LoginPage from '../Pages/LoginPage';
+import RegisterPage from '../Pages/RegisterPage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
-  '/new': NewPage,
+  '/login': LoginPage,
+  '/register': RegisterPage,
 };
 
 export default routes;
