a0bce246c937b8ecf4839d5eb46e2f53e3fde9a8
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2023-11-28 12:44:30 +0100

    Adding navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,50 +1,68 @@
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
+import IndexPage from '../Pages/HomePage';
+import LoginPage from '../Pages/LoginPage';
+import RegisterPage from '../Pages/RegisterPage';
 
-/**
- * Render the Navbar which is styled by using Bootstrap
- * Each item in the Navbar is tightly coupled with the Router configuration :
- * - the URI associated to a page shall be given in the attribute "data-uri" of the Navbar
- * - the router will show the Page associated to this URI when the user click on a nav-link
- */
+import logoImage from '../../img/logo1.png';
 
 const Navbar = () => {
-  const navbarWrapper = document.querySelector('#navbarWrapper');
-  const navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Add your brand here</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/game">Game</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/login">LoginPage</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/register">RegisterPage</a>
-              </li>                         
-            </ul>
-          </div>
-        </div>
-      </nav>
-      `;
-  navbarWrapper.innerHTML = navbar;
+  renderNavbar();
+  onNavBarClick();
+}
+
+function renderNavbar() {
+  const navbar = document.querySelector('#navbarWrapper');
+  navbar.innerHTML = `
+  <nav class="navbar navbar-expand-lg me-auto">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="${logoImage}" style="height:50px; width:50px;" alt="Home" data-uri="/"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="navbutton nav-link active white-text" aria-current="page" href="#" data-uri="/game">Game</a>
+          </li>
+          <li class="nav-item">
+            <a class="navbutton nav-link active white-text" aria-current="page" href="#" data-uri="/leaderboard">Leaderboard</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-right">
+         <li class="nav-item">
+           <a class="navbutton nav-link active white-text" href="#" data-uri="/login">Login</a>
+         </li>
+         <li class="nav-item">
+           <a class="navbutton nav-link active white-text" href="#" data-uri="/register">Register</a>
+         </li>
+       </ul>
+      </div>
+    </div>
+  </nav>
+
+  `;
+};
+
+function onNavBarClick() {
+  const navItems = document.querySelectorAll('.nav-link');
+
+  navItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      // eslint-disable-next-line no-console
+      console.log(`click on ${e.target.innerHTML} navbar item`);
+      if (e.target.innerHTML === '') {
+        IndexPage();
+      // eslint-disable-next-line spaced-comment
+      } /*else if (e.target.innerHTML === 'Leaderboard') {
+        LeaderboardPage();
+      }*/ else if (e.target.innerHTML === 'Login') {
+        LoginPage();
+      } else if (e.target.innerHTML === 'Register') {
+        RegisterPage();
+      }
+    });
+  });
 }
 
 export default Navbar;
