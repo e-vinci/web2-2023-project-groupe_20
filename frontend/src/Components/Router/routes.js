3d3c830661ffc3a817a3f6d0575a268ae2058ecc
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2023-12-13 12:51:43 +0100

    login/logout working - adaptive navbar (wip)

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -2,13 +2,14 @@ import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
-
+import LogoutPage from '../Pages/LogoutPage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/login': LoginPage,
   '/register': RegisterPage,
+  '/logout' : LogoutPage
 };
 
 export default routes;
