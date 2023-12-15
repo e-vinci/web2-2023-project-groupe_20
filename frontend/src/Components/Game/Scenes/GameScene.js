73aca39053c9b651d815f995e8c4b633ee8e6f62
Trinite Quiambao <roemer.quiambao@student.vinci.be>
2023-12-15 20:34:20 +0100

    modif: change number of player lives

--- frontend/src/Components/Game/Scenes/GameScene.js
+++ frontend/src/Components/Game/Scenes/GameScene.js
@@ -36,7 +36,7 @@ class GameScene extends Phaser.Scene {
         this.background.setOrigin(0,0);
         this.map = placementTilesData;
         this.props();
-        this.playerLives = 1;
+        this.playerLives = 10;
         this.nextWaveTime = 0;
         this.gameSpeed = 1;
         this.uiContainer = this.add.container(this.game.config.width / 2, 20);
