e9dd5203f82a0cfabce948c1defc1fdf14e5ef1c
MomzMc <mohamed.mcheik@student.vinci.be>
2023-12-16 13:57:19 +0100

    empty commit

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,14 @@
 const HomePage = () => {
     const main = document.querySelector('main');
     main.innerHTML = 'Deal with the content of your HomePage';
+    renderHomePage()
   };
+
+
+  function renderHomePage(){
   
+    const homePage = ``
+  }
+
+
   export default HomePage;
