d6ea84402ebd8a65f62e8802314919f87273d1c4
MohamedTouali <mohamed.touali@student.vinci.be>
2023-12-16 21:15:10 +0100

    Leaderboard backend et frontend fonctionnel

--- api/routes/scores.js
+++ api/routes/scores.js
@@ -4,31 +4,34 @@ const { authorize, isAdmin } = require('../utils/auths');
 
 const router = express.Router();
 
-// Read all scores from the leaderboard
+// Read best scores from the leaderboard
 router.get('/', async (req, res) => {
   const orderByScore = req?.query?.order?.includes('score') ? req.query.order : undefined;
   let orderedLeaderboard;
 
-  const scores = await Score.find();
+  const bestScores = await Score.readBestScores();
 
   if (orderByScore) {
-    orderedLeaderboard = [...scores].sort((a, b) => a.score - b.score);
+    orderedLeaderboard = [...bestScores].sort((a, b) => a.bestScore - b.bestScore);
   }
 
   if (orderByScore === '-score') {
     orderedLeaderboard = orderedLeaderboard.reverse();
   }
 
-  res.json(orderedLeaderboard ?? scores);
+  return res.json(orderedLeaderboard ?? bestScores);
 });
 
 // Create a score to be added to the leaderboard
 router.post('/', authorize, async (req, res) => {
   const { username, wave, score } = req.body;
 
-  // Validation des types pour wave et score
-  if (!username) {
-    return res.status(400).send('Bad Request');
+  if (typeof username !== 'string' || typeof wave !== 'number' || typeof score !== 'number') {
+    return res.status(400).send('Bad Request: Invalid data types');
+  }
+
+  if (wave < 0 || score < 0) {
+    return res.status(400).send('Bad Request: Waves and scores must be non-negative');
   }
 
   const createdScore = await Score.createOneScore(username, wave, score);
@@ -39,7 +42,7 @@ router.post('/', authorize, async (req, res) => {
 router.delete('/:id', authorize, isAdmin, async (req, res) => {
   const { id } = req.params;
 
-  const deletedScore = await Score.findByIdAndRemove(id);
+  const deletedScore = await Score.deleteOneScore(id);
 
   if (!deletedScore) {
     return res.status(404).send('Score not found');
